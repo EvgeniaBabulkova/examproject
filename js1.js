cdb01c3a628772f934f9ce28bf02a63be3bbfd0c
EvgeniaBabulkova <113165892+EvgeniaBabulkova@users.noreply.github.com>
2023-02-02 01:52:19 +0100

    First

---
+++ js1.js
@@ -0,0 +1,23 @@
+/**
+ * JavaScript: Multipart Form
+ */
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
---
+++ js2.js
@@ -0,0 +1,3 @@
+/**
+ * JavaScript: Multipart Form
+ */
